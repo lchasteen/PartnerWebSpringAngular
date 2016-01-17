927eaa249bbfc6cd7e42262a2e33085c0d6a7def
Lane Chasteen <lchasteen@gmail.com>
2016-01-16 21:57:21 -0500

    Add test and resources.

---
+++ src/main/test/com/partner/core/rest/TestRestClient.java
@@ -0,0 +1,14 @@
+package com.partner.core.rest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRestClient {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
